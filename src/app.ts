f16f756a406d207c603377142518f47a55b013cd
shinkouu <pham_kevin@outlook.de>
2025-04-03 18:08:58 +0200

    first init

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+/*
+    Title: Weather App
+    Brief description of the program: Basic weather app 
+
+    Name of the author: Kevin Pham
+    Date of creation 03.04.2025
+    Datum of last edit: 03.04.2025
+*/
